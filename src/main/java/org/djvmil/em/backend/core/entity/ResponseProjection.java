6cb990bddb53dcb0112e70b4425bb276b1b347dd
moustaphas.dieme <ndieme16@hotmail.com>
2024-03-29 18:18:47 +0100

    add Projection

--- src/main/java/org/djvmil/em/backend/core/entity/ResponseProjection.java
+++ src/main/java/org/djvmil/em/backend/core/entity/ResponseProjection.java
@@ -1,4 +1,10 @@
 package org.djvmil.em.backend.core.entity;
 
-public class ResponseProjection {
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "nameResponse", types = {Response.class})
+public interface ResponseProjection {
+    public Question getQuestion();
+    public String getResponseText();
+    public Long getScore();
 }
---
+++ src/main/java/org/djvmil/em/backend/core/entity/UserProjection.java
@@ -0,0 +1,6 @@
+package org.djvmil.em.backend.core.entity;
+
+import org.springframework.data.rest.core.config.Projection;
+
+
+
