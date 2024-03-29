bdce9c23e3d5f395d2868922aa77ec7084ba16a4
moustaphas.dieme <ndieme16@hotmail.com>
2024-03-29 18:41:27 +0100

    add Projection

--- src/main/java/org/djvmil/em/backend/core/entity/UserProjection.java
+++ src/main/java/org/djvmil/em/backend/core/entity/UserProjection.java
@@ -2,5 +2,9 @@ package org.djvmil.em.backend.core.entity;
 
 import org.springframework.data.rest.core.config.Projection;
 
-
-
+@Projection(name = "nameUser", types = {User.class})
+public interface UserProjection {
+    public String getFirstname();
+    public String getLastname();
+    public String getEmail();
+}
