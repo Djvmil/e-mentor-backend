2144645890b2c388bd2221dafd04ba72a20c5f6b
moustaphas.dieme <ndieme16@hotmail.com>
2024-04-02 03:31:32 +0200

    test cors

--- src/main/java/org/djvmil/em/backend/BackendApplication.java
+++ src/main/java/org/djvmil/em/backend/BackendApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication implements CommandLineRunner {
@@ -34,5 +36,15 @@ public class BackendApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		restConfiguration.exposeIdsFor(Response.class);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://51.75.27.211:8989");
+			}
+		};
 	}
 }
